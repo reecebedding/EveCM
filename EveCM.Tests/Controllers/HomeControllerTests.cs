4bb30d58fc2eb0e65e04b9bba2f878a77fa3a82f
Reece Bedding <reecebedding@hotmail.co.uk>
2018-05-17 21:06:32 +0100

    Add home controller test

---
+++ EveCM.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using EveCM.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveCM.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index_Should_Return_View()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+    }
+}
