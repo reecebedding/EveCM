88662f13178c7390e1d3d93a61ee8f5ac18190f0
Reece Bedding <reece.bedding@digital.voa.gov.uk>
2018-06-29 15:55:39 +0100

    Add support for redux-dev-tools

--- EveCM/ClientApp/app/store/configureStore.ts
+++ EveCM/ClientApp/app/store/configureStore.ts
@@ -2,11 +2,14 @@
 import rootReducer from '../reducers/rootReducer';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default function configureStore(initialState: {}) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
+        composeWithDevTools(
+            applyMiddleware(thunk, reduxImmutableStateInvariant())
+        )
     );
 }
